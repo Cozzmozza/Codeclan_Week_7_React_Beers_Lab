5e389926d4e1d49875ba49200776797f11cf6198
Colin Morrison <colinjmmorris@gmail.com>
2021-04-21 19:53:12 +0100

    Add/Remove favourites fixed

--- src/components/BeerDetail.js
+++ src/components/BeerDetail.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const BeerDetail = ({selectedBeer, beerChecker}) => {
 
-    const handleClick = () => {
-        beerChecker({selectedBeer})
+    const handleClick = (faveBeer) => {
+        beerChecker(faveBeer)
     }
 
     return(
@@ -11,9 +11,10 @@ const BeerDetail = ({selectedBeer, beerChecker}) => {
         <h3>Beer name: {selectedBeer.name}</h3>
         <p>Description: {selectedBeer.description}</p>
         <p>ABV: {selectedBeer.abv}</p>
-        <img src={selectedBeer.image_url} alt={`A picture of ${selectedBeer.name}`}></img>
+        <img src={selectedBeer.image_url} alt={` of ${selectedBeer.name}`}></img>
 
-        <button onClick={handleClick}>Add/Remove from favourites</button>
+        <button onClick={()=>handleClick(selectedBeer)}>Add/Remove from favourites</button>
+        {/* Button also works if we just do onClick={handleClick} and const handleclick = () */}
         </div>
     )
 }
--- src/containers/BeerContainer.js
+++ src/containers/BeerContainer.js
@@ -20,34 +20,42 @@ const BeerContainer = () => {
 
     const onSelectedBeer = (beer) => setSelectedBeer(beer);
 
-    const beerChecker = (beerToCheck) => {
-        const found = favouriteBeers.find((beer) => {
-            return beer.name === beerToCheck.name
-        })
-        if (found === true){
-            removeFavouriteBeer(beerToCheck)
-        }
-        else {addFavouriteBeer(beerToCheck)
-        }
-    };
-
     const addFavouriteBeer = (newFavouriteBeer) => {
-        const updatedFavouriteBeers = [...favouriteBeers, newFavouriteBeer];
-        setFavouriteBeers(updatedFavouriteBeers);
+        setFavouriteBeers([...favouriteBeers, newFavouriteBeer]);
     }
 
     const removeFavouriteBeer = (favouriteBeer) => {
-        const updatedFavouriteBeers = favouriteBeers.splice(favouriteBeer.index, 1);
-        setFavouriteBeers(updatedFavouriteBeers);
+        // const beerIndex = favouriteBeers.indexOf(favouriteBeer);
+        // favouriteBeers.splice(beerIndex, 1);
+        // console.log(`REMOVED ${favouriteBeer.name}`)
+        // setFavouriteBeers(favouriteBeers);
+        // The above seemed to work, but the favourites list only updated when I clicked on another beer..
+
+        const newFavourites = favouriteBeers.filter(beer => {
+            return beer.name !== favouriteBeer.name
+        })
+        setFavouriteBeers(newFavourites);
     }
+
+    const beerChecker = (beer => {
+        const found = favouriteBeers.find((beerCheck) => {
+            return beerCheck.name === beer.name;
+        })
+        // found ? removeFavouriteBeer(beer) : addFavouriteBeer(beer)
+        if (found){
+            removeFavouriteBeer(beer)
+        }
+        else {
+            addFavouriteBeer(beer)
+        }
+    });
 
     return(
         <div>
             <BeerSelect beers={beers} onSelectedBeer={onSelectedBeer}/>
             {selectedBeer ? <BeerDetail selectedBeer={selectedBeer} beerChecker={beerChecker}/> : null}
 
-            {favouriteBeers.length > 0 ? <BeerFavourites favouriteBeers={favouriteBeers} /> : null}
-        
+            {favouriteBeers.length > 0 ? <BeerFavourites favouriteBeers={favouriteBeers} /> : null}        
         </div>
     )
 }
